cc9867d7a8d16c605bd4f75f275595d7ac1f4cf7
mish24 <poonammishra24398@gmail.com>
2017-07-07 18:36:15 +0530

    stopped re-usage of emails

--- oshc/main/models.py
+++ oshc/main/models.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+User._meta.get_field('email')._unique = True
